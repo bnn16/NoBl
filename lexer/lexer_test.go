85ad2afab188b0dca09570bd083553effbb55c53
Bnn16 <Bogdan.Nikolov4@outlook.com>
2025-03-31 03:59:14 +0200

    test: add tests for simple tokens

---
+++ lexer/lexer_test.go
@@ -0,0 +1,44 @@
+package lexer
+
+import (
+	"go/printer"
+	"nobl/token"
+	"testing"
+)
+
+func TestSimpleToken(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LEFT_PAREN, "("},
+		{token.RIGHT_PAREN, ")"},
+		{token.LEFT_BRACE, "{"},
+		{token.RIGHT_BRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		printer.Fprint(nil, nil, tok)
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
